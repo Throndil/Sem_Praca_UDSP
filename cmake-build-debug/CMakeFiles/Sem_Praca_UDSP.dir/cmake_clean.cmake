894dced7653dab98cc4a1194f2a7f3b8e82614fd
Throndil <urbanek.m08@gmail.com>
2021-12-29 23:29:40 +0100

    Test

---
+++ cmake-build-debug/CMakeFiles/Sem_Praca_UDSP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sem_Praca_UDSP.dir/main.c.o"
+  "Sem_Praca_UDSP"
+  "Sem_Praca_UDSP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sem_Praca_UDSP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/testgitu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testgitu.dir/main.c.o"
+  "CMakeFiles/testgitu.dir/table.c.o"
+  "testgitu"
+  "testgitu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testgitu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
